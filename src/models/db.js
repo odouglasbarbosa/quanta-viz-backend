4b11bfb9aeaf075d6fe53872a2a7ffe6443fa6f1
odouglasbarbosa <douglasdbj@gmail.com>
2025-02-08 23:05:36 -0300

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'quantaviz-db.c92usc8as5uf.us-east-2.rds.amazonaws.com', // Endpoint do RDS (ex: quantaviz-db.xxxxxxxxxxxx.us-east-1.rds.amazonaws.com)
     user: 'admin', // Nome de usuário do banco de dados
-    password: '13121961*Lm#', // Senha do banco de dados
+    password: '932802*Db#', // Senha do banco de dados
     database: 'quantaviz_db' // Nome do banco de dados
 });
 module.exports = connection;
